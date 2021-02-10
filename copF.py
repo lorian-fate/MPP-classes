a0661e18979d8c2a99d51dc282520cea17957ed5
lorian-fate <ta01001011@gmail.com>
2021-02-10 16:30:48 +0100

    today commit

---
+++ copF.py
@@ -0,0 +1,27 @@
+from os import system
+import shutil
+
+shutil.copy("F:\ABSENTIA", "C:\Users\cice\Desktop\PRUEBA\DESTINO")
+#system('dir')
+#system('cd..')
+#system('cd..')
+#system('cd..')
+#system('cd..')
+#system('cd..')
+#system('cd..')
+#system('cd..')
+#system('cd windows/system32')
+#system("robocopy F:\ABSENTIA C:\Users\cice\Desktop\PRUEBA\DESTINO")
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mpp_09022021.py
+++ mpp_09022021.py
@@ -1,3 +1,11 @@
+Crear ejecutable en python
+1._ instalar pyinstaller| pip install pyinstaller
+2._ head hover to the directory where the app it is
+3._ whire pyinstaller app_name or | pyinstaller --windowed app_name.py or | pyinstaller --windowed --onefile app_name.py
+
+
+MODULO DE AUTOMATIZACION MULTIPLATAFORMA
+https://recursospython.com/guias-y-manuales/pyautogui/#comment-846
 
 file_saved = open('new_file.txt', 'w')
 
